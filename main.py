34c8f42af850a3dbdd596b012f651183d75c82f8
ching <chinghei1007@gmail.com>
2024-04-08 16:25:39 +0800

    0408 1624

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import email.utils
 import socket
 import os
 import datetime
+import time
+from time import gmtime,strftime
 import mimetypes
-import threading
 from concurrent.futures import ThreadPoolExecutor
 
 
@@ -14,7 +15,9 @@ def file_send(path, conn, request_headers):
         response = "HTTP/1.1 404 File Not Found \r\n\r\n"
         f = open('html/404.html')
         file_data = f.read()
-        conn.sendall((response+file_data).encode())
+        conn.sendall((response + file_data).encode())
+        # log
+        write_log("404 Not Found\n")
         return
 
     if path == 'html/':
@@ -28,9 +31,11 @@ def file_send(path, conn, request_headers):
         client_last_modified = request_headers.get("If-Modified-Since")
         if client_last_modified == last_modified:
             # 304 Not modified
-            response = b"HTTP/1.1 304 Not Modified\r\n\r\n"
+            response = "HTTP/1.1 304 Not Modified\r\n\r\n"
+            # log
+            write_log("304 Not Modified\n")
             print(response)
-            conn.sendall(response)
+            conn.sendall(response).encode()
             return
 
     # 200 OK header
@@ -40,18 +45,19 @@ def file_send(path, conn, request_headers):
     header += f"Content-Length: {len(file_data)}\r\n".encode()
     header += f"Last-Modified: {last_modified}\r\n".encode()
     header += b"\r\n"
-    write_log(header+file_data)
+    write_log("200 OK\n")
     conn.sendall(header + file_data)
+    conn.close()
 
 
-def head_function(path, conn, request_headers):
+def head_function(path, connection, request_headers):
     path = 'html' + path
     # 404 Not Found
     if not os.path.exists(path):
         response = "HTTP/1.1 404 File Not Found\r\n\r\n"
         f = open('html/404.html')
         file_data = f.read()
-        conn.sendall((response + file_data).encode())
+        connection.sendall((response + file_data).encode())
         return
 
     if path == 'html/':
@@ -59,42 +65,49 @@ def head_function(path, conn, request_headers):
 
     last_modified = get_last_modified(path)
     content_type = get_extension_type(path)
-
     # 200 OK header
     header = "HTTP/1.1 200 OK\r\n"
     header += f"Content-Type: {content_type}\r\n"
     header += f"Last-Modified: {last_modified}\r\n"
     header += "\r\n"
     write_log(header)
-    conn.sendall(header.encode())
-    conn.close()
+    connection.sendall(header.encode())
+    connection.close()
 
-def request_rcv(conn,request):
+
+def request_rcv(connection, request, address):
     command = request.split()[0]
     path = request.split()[1]
     print(path)
+    if path == "/":
+        path = "/index.html"
+    print(time.ctime())
+    log_text = str(address[0]) + ":" + str(address[1]) + "|" + str(time.ctime()) + "|" + request.split(" ")[0] + request.split(" ")[1] + "|"
+    write_log(log_text)
     headers = {}
     lines = request.splitlines()
     for line in lines[1:]:
         if not line.strip():
             break
-        head,value = line.split(': ', 1)
+        head, value = line.split(': ', 1)
         headers[head] = value
-    if command == 'HEAD': #HEAD
-        head_function(path,conn,headers)
-    else: #GET
-        file_send(path,conn,headers)
+    if command == 'HEAD':  # HEAD
+        head_function(path, connection, headers)
+    else:  # GET
+        file_send(path, conn, headers)
     conn.close()
 
+
 def write_log(text):
-    with open('log/log.txt','a+') as file:
-        file.write(request + '\n')
+    with open('log/log.txt', 'a+') as file:
+        file.write(text)
+        return
+
 
 def get_last_modified(path):
     timestamp = os.path.getmtime(path)
-    l_modi = datetime.datetime.utcfromtimestamp(timestamp)
-    l_modi_str = email.utils.formatdate(timeval=l_modi.timestamp(),localtime=False,usegmt=True)
-    return l_modi_str
+    print(timestamp)
+    return timestamp
 
 
 def get_extension_type(path):
@@ -105,9 +118,9 @@ def get_extension_type(path):
 ip = '127.0.0.1'
 port = 8080
 
-skt = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-skt.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-skt.bind((ip,port))
+skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+skt.bind((ip, port))
 skt.listen(5)
 print('Listening on port ', port)
 
@@ -115,11 +128,9 @@ pool = ThreadPoolExecutor(max_workers=5)
 while True:
     conn,addr = skt.accept()
 
-    request = conn.recv(1024).decode()
+    request = conn.recv(1024).decode('utf-8')
     if not request:
         continue
     print(request)
-    write_log(request)
-
-    pool.submit(request_rcv, conn, request)
+    pool.submit(request_rcv, conn, request, addr)
 skt.close()
